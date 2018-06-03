7e256aedfc109bcba781afc3e24e88a6a7bb941d
pmm0316 <847679250@qq.com>
2018-06-03 21:22:56 +0800

    音乐播放器项目 创建vue-cli，完成header组件

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div id="app">
+    <m-header></m-header>
+    hello word
+  </div>
+</template>
+
+<script>
+import MHeader from 'components/header/MHeader'
+export default {
+  name: 'App',
+  components: {
+    MHeader
+  }
+}
+</script>
+
+<style lang="stylus">
+// 引入样式，用@import方式引入
+@import '~stylus/variable.styl'
+#app
+  color $color-theme
+</style>
---
+++ src/components/header/MHeader.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="mHeader">
+      <div class="icon"></div>
+      <h1 class="text">Music</h1>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MHeader'
+}
+</script>
+
+<style scoped lang='stylus' type='text/stylus'>
+@import "~stylus/variable.styl"
+@import "~stylus/mixin.styl"
+.mHeader
+  position relative
+  height 44px
+  text-align center
+  font-size 0
+  .icon
+    display inline-block
+    vertical-align top
+    margin-top 6px
+    height 32px
+    width 30px
+    margin-right 9px
+    bg-image('logo')
+    background-size 30px 32px
+  .text
+    display inline-block
+    vertical-align top
+    line-height 44px
+    font-size $font-size-large
+</style>
