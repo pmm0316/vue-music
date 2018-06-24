c448b277b1a566118ecabf1fc4b881c228f7d367
pmm0316 <847679250@qq.com>
2018-06-24 12:10:48 +0800

    在store中添加了播放器相关的参数

---
+++ src/common/js/config.js
@@ -0,0 +1,9 @@
+/**
+ * CREATED DATE: 2018/6/23 22:02:57
+ * author: Absent Min
+ */
+export const playMode = {
+  sequence: 1,
+  loop: 2,
+  random: 3
+}
--- src/store/state.js
+++ src/store/state.js
@@ -2,8 +2,15 @@
  * CREATED DATE: 2018/6/21 20:50:27
  * author: Absent Min
  */
+import {playMode} from 'common/js/config'
 const state = {
-  singer: {}
+  singer: {},
+  playing: false,
+  fullScreen: false,
+  playList: [],
+  sequenceList: [],
+  mode: playMode.sequence,
+  currentIndex: -1
 }
 
 export default state
